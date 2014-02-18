848ee09acee941ac47e8b55ab141d7b7712f2f7e
Dustin Perzanowski <perzanowski.4@osu.edu>
2014-02-18 12:57:01 -0500

    add regex to remove parens around audio file links

--- audio_to_player.js
+++ audio_to_player.js
@@ -1,6 +1,20 @@
 jQuery(document).ready(function($) {
 	$('a[href$=".mp3"]').each(function(index) {
+
+		// "(" regex pattern
+		var pattern_1 = /(\s)*\((\s)*/;
+		if (pattern_1.exec($(this.previousSibling).text())) {
+			$(this.previousSibling).remove();
+		}
+
+		// ")" regex pattern
+		var pattern_2 = /(\s)*\)(\s)*/;
+		if (pattern_2.exec($(this.nextSibling).text())) {
+			$(this.nextSibling).remove();
+		}
+
 		$(this).replaceWith('<audio preload="auto"><source src="' + this.href + '"></audio>');
+
 	});
 });
 
